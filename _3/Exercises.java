cd5a42eab59eea22c3716faa16520f592f08392c
Davide <davide.noto.work@gmail.com>
2023-03-29 19:51:00 +0200

    aggiunto esercizio_3 da completare

--- _3/Exercises.java
+++ _3/Exercises.java
@@ -10,16 +10,19 @@ public class Exercises {
     }
 
 
-     /**
+    /**
      * 1: Complete the function that removes any duplicates by using a set
-     *    then recreates a new list of unique items
-      *
-      *   You can reset a list by using list = new ArrayList<>();
+     * then recreates a new list of unique items
+     * <p>
+     * You can reset a list by using list = new ArrayList<>();
      */
     private static void exercise1() {
         List<String> list = new ArrayList<>(Arrays.asList("a", "b", "c", "a", "b", "c", "a", "b", "c"));
 
-        // Your code here
+        // inserisco la lista in un set così i doppioni vengono automaticamente rimossi;
+        Set<String> sdoppionaotre = new HashSet<>(list);
+        // infine riassegno a list il set;
+        list = new ArrayList<>(sdoppionaotre);
 
         if (list.size() == 3) {
             System.out.println("Exercise 1: Success");
@@ -30,18 +33,39 @@ public class Exercises {
 
     /**
      * 2: Write a method that takes a List of Strings or a Set of Strings and returns a Set of Strings.
-     *    the method should take a collection as a parameter
+     * the method should take a collection as a parameter
      */
     private static void exercise2() {
         // Your code here
         List<String> list = Arrays.asList("apple", "banana", "apple", "orange", "kiwi", "banana");
         Set<String> set = Set.of("apple", "banana");
-        // toSet(list); <-- uncomment
-        // toSet(set); <-- uncomment
+
+        // richiesta dell'esercizio;
+        Set<String> listaConvertita = toSet(list);
+        Set<String> setConvertito = toSet(set);
+
+        // prova per vedere se funziona;
+        List<String> listaProva = Arrays.asList("0", "1", "2", "3");
+        Set<String> merge2Collezioni = twoListToSet(list, listaProva);
+
+        // test
+        System.out.println(listaConvertita);
+        System.out.println(setConvertito);
+        System.out.println(merge2Collezioni);
+
     }
 
-    // Write your method for exercise 2 here
+    // ho creato un metodo che accetta una collezione e restituisce un SET per rimuovere i doppioni;
+    private static Set<String> toSet(Collection<String> collezione) {
+        return new HashSet<>(collezione);
+    }
 
+    // PROVA: secondo metodo di prova che accetta 2 parametri di tipo collezione;
+    private static Set<String> twoListToSet(Collection<String> collezione, Collection<String> collezione1) {
+        Set<String> liste = new HashSet<>(collezione);
+        liste.addAll(collezione1);
+        return liste;
+    }
 
     /**
      * 3: Follow the instructions in the comments
@@ -49,13 +73,36 @@ public class Exercises {
     private static void exercise3() {
         String str = "the fox and the lazy dog saw another fox with another not lazy dog";
         List<String> words = Arrays.stream(str.split(" ")).toList();
+
+//         Write code to count the number of occurrences of each word in the String `words`
+//         and store the counts in the Map `wordCount`, note you will need to first get the previous value
+//         from the Map and then increment it by 1, if no previous value exists, use 0 as the previous value.
+
+
+        // collezione con i counter;
+        System.out.println("Contatore ripetizione parole: \n" + contaParole(words));
+
+
+        // collezione senza doppioni tanto per;
+        Set<String> wordsNoDopp = new HashSet<>(words);
+        System.out.println("Collezione senza doppioni: \n" + wordsNoDopp);
+    }
+
+    private static Map<String, Integer> contaParole(List<String> collezione) {
+
+        // creo una mappa in cui inserire le Key(parole) e Value(quante volte si ripete la stessa parola);
         Map<String, Integer> wordCount = new HashMap<>();
 
-        // Write code to count the number of occurrences of each word in the String `words`
-        // and store the counts in the Map `wordCount`, note you will need to first get the previous value
-        // from the Map and then increment it by 1, if no previous value exists, use 0 as the previous value.
-
-        // Your code here
+        // itero all'interno della collezione
+        for (int i = 0; i < collezione.size(); i++) {
+            // pesco la parola corrente del ciclo;
+            String parola = collezione.get(i);
+            // assegno a "ripetizione" il valore di indice della parola stessa (es. se gli indici sono 2 allora diventa 2);
+            int ripetizione = wordCount.getOrDefault(parola, 0);
+            // inserisco nella mappa la parola e il numero di ripetizione +1 perchè in default è 0;
+            wordCount.put(parola, ripetizione + 1);
+        }
+        return wordCount;
     }
 
     /**
